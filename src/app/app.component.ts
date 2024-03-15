f67f4fcf15fbcc0cc3655c76f11e694ac363c318
Huynh Thanh Liem <18520982@gm.uit.edu.vn>
2024-03-15 14:57:55 +0700

    config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import liff from '@line/liff';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true
 })
-export class AppComponent {
-  title = 'angular-line-login-v2';
+export class AppComponent implements OnInit {
+  title = 'angular-line-login';
+  idToken: string | null = '';
+  displayName: string | undefined = '';
+  pictureUrl: string | undefined = '';;
+  statusMessage: string | undefined = '';
+  userId: string | undefined = '';
+
+  ngOnInit(): void {
+    this.initLine();
+  }
+
+  initLine(): void {
+    liff.init({ liffId: '1655250022-AVJylL18' }, () => {
+      if (liff.isLoggedIn()) {
+        this.runApp();
+      } else {
+        liff.login();
+      }
+    }, err => console.error(err));
+  }
+
+  runApp(): void {
+    const idToken = liff.getIDToken();
+    this.idToken = idToken;
+    liff.getProfile().then(profile => {
+      console.log(profile);
+      this.displayName = profile.displayName;
+      this.pictureUrl = profile.pictureUrl;
+      this.statusMessage = profile.statusMessage;
+      this.userId = profile.userId;
+    }).catch(err => console.error(err));
+  }
+
+  logout(): void {
+    liff.logout();
+    window.location.reload();
+  }
 }
